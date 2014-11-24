78ea607d98e525666bbf60a012f022757d41576a
Sergio Martin <smbalsa@gmail.com>
2014-11-24 15:26:12 +0100

    upgrade parsing noticias

--- news-marca-parsing.rb
+++ news-marca-parsing.rb
@@ -4,54 +4,74 @@
 require 'open-uri'
 require 'nokogiri'
 
+require 'rubygems'
+require 'json'
+require 'net/http'
 
-def createArticles (page)
+
+def createArticles (ficheros, urls, nxt, id)
+  File.new("#{ficheros}", "w")
+  
+  html = open("#{urls}").read
+  article = Nokogiri::HTML(html)
+
+  File.open("#{ficheros}", "w:UTF-8") do |f2|
+  
+    header = article.at_css(".desarrollo header h1").text()
+    resume = article.at_css(".desarrollo header .subtitulos")
+    photo = article.at_css("#fotoPrincipal")['src']
+    altphoto = article.at_css("div.foto img")['alt']
+    sign = article.at_css(".desarrollo div.firma span")
+    fecha = article.at_css(".desarrollo time")
+    
+    text = article.at_css(".desarrollo div#tamano")
+    
+    
+    
+ 
+    puts header
+    f2.puts "<section id='#{id}'>"
+    f2.puts "<h1>#{header}</h1>"
+    f2.puts "#{resume}"
+    f2.puts "<div class='foto'><img src='#{photo}' alt='#{altphoto}' /></div>"
+    f2.puts "#{sign}"
+    f2.puts "#{fecha}"
+    f2.puts "<div class='ads'><img class='img-responsive' src='http://placehold.it/300x250'></div>"
+    f2.puts "#{text}"
+    f2.puts "</section>"
+    f2.puts "<a href='noticias/#{nxt}.html' title='#{nxt}'>Siguiente</a>"
+    
+  end
+end
+
+def readArticles (page)
   
   html = open("#{page}").read
   article = Nokogiri::HTML(html)
+   #sample = Array.new 
+  article.search("section#relatedNews ul li").each do |x|
+    site_article = x.at_css("a")['href']
+    site_article_title = x.at_css("a[href]").text
+    dominio = "http://www.elmundo.es"
+    
+    myfile = "#{site_article}".split('/')[-1].split('=')[-1]
+    
+    allmyfiles = "/Users/Leo/Sites/Demo-Scrolling-Ads/noticias/#{myfile}.html"   
+    
+    #sample.push myfile
+    
+    nextpage = x.next_element.at_css("a")['href'].split('=')[-1]
+    
   
-  article.search("h5").each do |x|
-    marca_article = x.at_css("a")['href']
-    marca_article_title = x.at_css("a[href]").text
-    dominio = "http://www.marca.com"
+    createArticles(allmyfiles, site_article, nextpage, myfile)
     
-    myfile = "#{marca_article}".split('/')[-1]
+
     
-    allmyfiles = "/Users/Leo/Sites/Demo-Scrolling-Ads/noticias/#{myfile}"
-    
-    #puts "#{marca_article_title}"
-    #puts "#{allmyfiles}"
-     
-    File.new("#{allmyfiles}", "w")
-    
-    html = open("#{dominio}#{marca_article}").read
-    article = Nokogiri::HTML(html)
-  
-    File.open("#{allmyfiles}", "r+") do |f2|
-    
-      header = article.at_css(".contenido_not h2").text()
-      sign = article.at_css(".contenido_not p.firma").text()
-      text = article.at_css(".contenido_not p").text()
-      photo = article.at_css(".contenido_not img")['src']
-    
-      myid = myfile.split(".")[0]
-      
-      puts myid
-    
-      f2.puts "<section id='noticia_#{myid}'>"
-      f2.puts "<h1>#{header}</h1>"
-      f2.puts "<small>#{sign}</small>"
-      f2.puts "<img src='#{photo}' />"
-      f2.puts "<p>#{text}</p>"
-      f2.puts "</section>"
-      f2.puts "<a href='noticias/#{myfile}' title='#{myid}'>Siguiente</a>"
-    end
     
   end
-
 end
 
 print "Listado de noticias: "
 url = gets.chomp
 
-createArticles ("#{url}")
+readArticles ("#{url}")
